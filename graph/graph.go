1f39443a6bf40c163e11114f88f2ce9505b1f44d
Dorin <dorin.fodor@gmail.com>
2019-12-26 21:47:43 +0000

    added graph

---
+++ graph/graph.go
@@ -0,0 +1,73 @@
+/*
+Package graph provides a Graph data structure (directed & weighted)
+*/
+package graph
+
+// Graph : represents a Graph
+type Graph struct {
+	nodes []*GraphNode
+}
+
+// GraphNode : represents a Graph node
+type GraphNode struct {
+	id    int
+	edges map[int]int
+}
+
+// New : returns a new instance of a Graph
+func New() *Graph {
+	return &Graph{
+		nodes: []*GraphNode{},
+	}
+}
+
+// AddNode : adds a new node to the Graph
+func (g *Graph) AddNode() (id int) {
+	id = len(g.nodes)
+	g.nodes = append(g.nodes, &GraphNode{
+		id:    id,
+		edges: make(map[int]int),
+	})
+	return
+}
+
+// AddEdge : adds a directional edge together with a weight
+func (g *Graph) AddEdge(n1, n2 int, w int) {
+	g.nodes[n1].edges[n2] = w
+}
+
+// Neighbors : returns a list of node IDs that are linked to this node
+func (g *Graph) Neighbors(id int) []int {
+	neighbors := []int{}
+	for _, node := range g.nodes {
+		for edge := range node.edges {
+			if node.id == id {
+				neighbors = append(neighbors, edge)
+			}
+			if edge == id {
+				neighbors = append(neighbors, node.id)
+			}
+		}
+	}
+	return neighbors
+}
+
+// Nodes : returns a list of node IDs
+func (g *Graph) Nodes() []int {
+	nodes := make([]int, len(g.nodes))
+	for i := 0; i < len(g.nodes); i++ {
+		nodes[i] = i
+	}
+	return nodes
+}
+
+// Edges : returns a list of edges with weights
+func (g *Graph) Edges() [][3]int {
+	edges := make([][3]int, 0, len(g.nodes))
+	for i := 0; i < len(g.nodes); i++ {
+		for k, v := range g.nodes[i].edges {
+			edges = append(edges, [3]int{i, k, int(v)})
+		}
+	}
+	return edges
+}
---
+++ graph/graph_test.go
@@ -0,0 +1,27 @@
+package graph
+
+import (
+	"fmt"
+)
+
+func ExampleGraph() {
+	graph := New()
+	node0 := graph.AddNode()
+	node1 := graph.AddNode()
+	node2 := graph.AddNode()
+
+	graph.AddEdge(node0, node1, 1)
+
+	fmt.Println(node0)
+	fmt.Println(node1)
+	fmt.Println(node2)
+	fmt.Println(graph.Nodes())
+	graph.AddEdge(node0, node2, 5)
+	fmt.Println(graph.Neighbors(node0))
+	// Output:
+	// 0
+	// 1
+	// 2
+	// [0 1 2]
+	// [1 2]
+}
